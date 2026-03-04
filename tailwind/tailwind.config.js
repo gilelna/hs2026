08f71e736d95c91c01f2bdf51ba56c5f0f595fc5
Gil Elnatan <gil@hadarshemesh.com>
2026-03-04 18:32:33 +0100

    Safelist margin/padding classes and extend spacing

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -8,8 +8,20 @@ module.exports = {
     "../includes/**/*.php",
     "../src/**/*.{js,ts,jsx,tsx,html}",
   ],
+  safelist: [
+    {
+      pattern: /^(p|px|py|pt|pr|pb|pl|m|mx|my|mt|mr|mb|ml)-(auto|\\d+)$/,
+    },
+  ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '10': '2.5rem',
+        '20': '5rem',
+        '24': '6rem',
+        '32': '8rem',
+      },
+    },
   },
   plugins: [],
 };
